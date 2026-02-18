a59ce74505723a3eb2cf8c6022cf7308d1953645
Your Name <you@example.com>
2026-02-18 22:50:41 +0700

    fix: update KisskhProvider to use Plugin with context for LicenseClient.init

--- KisskhProvider/src/main/kotlin/com/kisskh/KisskhProviderPlugin.kt
+++ KisskhProvider/src/main/kotlin/com/kisskh/KisskhProviderPlugin.kt
@@ -1,11 +1,15 @@
 package com.kisskh
 
-import com.lagradost.cloudstream3.plugins.BasePlugin
+import android.content.Context
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
 
 @CloudstreamPlugin
-class KisskhProviderPlugin: BasePlugin() {
-    override fun load() {
+class KisskhProviderPlugin: Plugin() {
+    override fun load(context: Context) {
+        LicenseClient.init(context)
+        val savedKey = settingsManager.getString("license_key", "")
+        if (savedKey.isNotEmpty()) LicenseClient.setLicenseKey(context, savedKey)
         registerMainAPI(KisskhProvider())
     }
 }
